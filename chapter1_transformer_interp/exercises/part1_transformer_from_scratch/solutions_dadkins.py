9ff22d85e90f60828573e6a118a67f3a30202402
dannyadkins <danielpadk@gmail.com>
2024-01-11 04:14:52 +0000

    get LayerNorm working

--- chapter1_transformer_interp/exercises/part1_transformer_from_scratch/solutions_dadkins.py
+++ chapter1_transformer_interp/exercises/part1_transformer_from_scratch/solutions_dadkins.py
@@ -64,4 +64,133 @@ if (MAIN):
         next_token = logits[0, -1].argmax(dim=-1)
         # Decode and print the result
         next_char = reference_gpt2.to_string(next_token)
+    
+# %%
+
+if (MAIN):
+    # Activations of reference model
+    for activation_name, activation in cache.items():
+        # Only print for first layer
+        if ".0." in activation_name or "blocks" not in activation_name:
+            print(f"{activation_name:30} {tuple(activation.shape)}")
+
+    for name, param in reference_gpt2.named_parameters():
+        # Only print for first layer
+        if ".0." in name or "blocks" not in name:
+            print(f"{name:18} {tuple(param.shape)}")
+
+# Output: 
+# hook_embed                     (1, 35, 768)
+# hook_pos_embed                 (1, 35, 768)
+# blocks.0.hook_resid_pre        (1, 35, 768)
+# blocks.0.ln1.hook_scale        (1, 35, 1)
+# blocks.0.ln1.hook_normalized   (1, 35, 768)
+# blocks.0.attn.hook_q           (1, 35, 12, 64)
+# blocks.0.attn.hook_k           (1, 35, 12, 64)
+# blocks.0.attn.hook_v           (1, 35, 12, 64)
+# blocks.0.attn.hook_attn_scores (1, 12, 35, 35)
+# blocks.0.attn.hook_pattern     (1, 12, 35, 35)
+# blocks.0.attn.hook_z           (1, 35, 12, 64)
+# blocks.0.hook_attn_out         (1, 35, 768)
+# blocks.0.hook_resid_mid        (1, 35, 768)
+# blocks.0.ln2.hook_scale        (1, 35, 1)
+# blocks.0.ln2.hook_normalized   (1, 35, 768)
+# blocks.0.mlp.hook_pre          (1, 35, 3072)
+# blocks.0.mlp.hook_post         (1, 35, 3072)
+# blocks.0.hook_mlp_out          (1, 35, 768)
+# blocks.0.hook_resid_post       (1, 35, 768)
+# ln_final.hook_scale            (1, 35, 1)
+# ln_final.hook_normalized       (1, 35, 768)
+# embed.W_E          (50257, 768)
+# pos_embed.W_pos    (1024, 768)
+# blocks.0.ln1.w     (768,)
+# blocks.0.ln1.b     (768,)
+# ...
+# ln_final.w         (768,)
+# ln_final.b         (768,)
+# unembed.W_U        (768, 50257)
+# unembed.b_U        (50257,)
+# %%
+
+# config
+
+@dataclass
+class Config:
+    d_model: int = 768
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
+
+# %%
+
+# tests 
+def rand_float_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randn(shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+def rand_int_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randint(100, 1000, shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+def load_gpt2_test(cls, gpt2_layer, input):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    layer.load_state_dict(gpt2_layer.state_dict(), strict=False)
+    print("Input shape:", input.shape)
+    output = layer(input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape)
+    try: reference_output = gpt2_layer(input)
+    except: reference_output = gpt2_layer(input, input, input)
+    print("Reference output shape:", reference_output.shape, "\n")
+    comparison = t.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+    print(f"{comparison.sum()/comparison.numel():.2%} of the values are correct\n")
+
+
+# %%
+
+# This is where the fun starts. These are my implementations:
+
+class LayerNorm(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.w = nn.Parameter(t.ones(cfg.d_model))
+        self.b = nn.Parameter(t.zeros(cfg.d_model))
+
+    # Make mean 0
+    # Normalize to have variance 1
+    # Scale with learned weights
+    # Translate with learned bias
+    def forward(self, residual: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+        # the keepdim preserves the d_model
+        mean = residual.mean(dim=-1, keepdim=True)
+        eps = self.cfg.layer_norm_eps
+        variance = residual.var(dim=-1, keepdim=True, unbiased=False)  
+        # learnable 
+        w = self.w
+        b = self.b
+
+        out = (residual - mean ) / t.sqrt(variance + eps) * w + b
+        print(out.shape)
+        return out          
+
+rand_float_test(LayerNorm, [2, 4, 768])
+load_gpt2_test(LayerNorm, reference_gpt2.ln_final, cache["resid_post", 11])
 # %%
