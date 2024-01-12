faebba3ac0c126741f1c3a1da7b3597a10c13c7f
dannyadkins <danielpadk@gmail.com>
2024-01-12 03:31:27 +0000

    Unembed ez

--- chapter1_transformer_interp/exercises/part1_transformer_from_scratch/solutions_dadkins.py
+++ chapter1_transformer_interp/exercises/part1_transformer_from_scratch/solutions_dadkins.py
@@ -369,7 +369,22 @@ class TransformerBlock(nn.Module):
 rand_float_test(TransformerBlock, [2, 4, 768])
 load_gpt2_test(TransformerBlock, reference_gpt2.blocks[0], cache["resid_pre", 0])
 # %%
+class Unembed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_U = nn.Parameter(t.empty((cfg.d_model, cfg.d_vocab)))
+        nn.init.normal_(self.W_U, std=self.cfg.init_range)
+        self.b_U = nn.Parameter(t.zeros((cfg.d_vocab), requires_grad=False))
 
+    def forward(
+        self, normalized_resid_final: Float[Tensor, "batch position d_model"]
+    ) -> Float[Tensor, "batch position d_vocab"]:
+        return normalized_resid_final @ self.W_U + self.b_U
+
+
+rand_float_test(Unembed, [2, 4, 768])
+load_gpt2_test(Unembed, reference_gpt2.unembed, cache["ln_final.hook_normalized"])
 # %%
 
 # %%
